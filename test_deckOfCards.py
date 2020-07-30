e24e41d7d6c004af81ed3e846fdc18476d256ebb
raphaelhazout <68278696+raphaelhazout@users.noreply.github.com>
2020-07-30 16:40:12 +0300

    this is game card project

---
+++ test_deckOfCards.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestDeckOfCards(TestCase):
+    def test_deal_one(self):
+        pass
+
+    def test_new_game(self):
+        pass
+
+    def test_show(self):
+        pass
